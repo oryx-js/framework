51b4bdf37f19e0ad4f774dbd09099181794fa913
Refkinscallv <205364309+oryx-js@users.noreply.github.com>
2025-04-23 14:58:27 +0700

    [Update] add a jwt core file

---
+++ src/core/jwt.ts
@@ -0,0 +1,48 @@
+/**
+ * packages
+ */
+import jwt, { 
+    SignOptions, 
+    JwtPayload 
+} from 'jsonwebtoken';
+import Common from '@core/common';
+
+class JWT {
+    private static secretKey: string = Common.env<string>('JWT_SECRET_KEY') || (() => { throw new Error('JWT_SECRET_KEY is not defined'); })();
+
+    private static expireIn: number = Common.env<number>('JWT_EXPIRE_IN', 86400);
+
+    static sign<T extends JwtPayload>(payload: T, options: SignOptions = {}): string {
+        return jwt.sign(payload, this.secretKey, {
+            expiresIn: this.expireIn,
+            ...options,
+        });
+    }
+
+    static verify<T = any>(token: string): T | null {
+        try {
+            return jwt.verify(token, this.secretKey) as T;
+        } catch (err: any) {
+            Common.logger('warn', 'JWT', 'Token verification failed');
+            return null;
+        }
+    }
+
+    static encode<T extends object>(payload: T): string {
+        return jwt.sign(payload, this.secretKey, {
+            algorithm: 'HS256',
+            noTimestamp: true,
+        });
+    }
+
+    static decode<T = any>(token: string): T | null {
+        try {
+            return jwt.decode(token) as T;
+        } catch (err: any) {
+            Common.logger('warn', 'JWT', 'Token decoding failed');
+            return null;
+        }
+    }
+}
+
+export default JWT;
